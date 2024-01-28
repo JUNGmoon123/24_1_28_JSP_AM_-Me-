eb7bf57ce2b29dd959681af4bf24d8baa99d7ff6
JUNGmoon123 <jungpro567@gmail.com>
2024-01-28 17:51:23 +0900

    게시물 리스트를 브라우저 에서 노출, 서블릿이 DB에서 가져온 데이터를 jsp에게 넘겨주고 jsp에서 출력하기

--- src/main/java/com/KoreaIT/java/jsp_AM_M/ArticleListesvlet.java
+++ src/main/java/com/KoreaIT/java/jsp_AM_M/ArticleListesvlet.java
@@ -21,21 +21,20 @@ public class ArticleListesvlet extends HttpServlet {
 		//한글로 나오게해줌
 		response.setContentType("text/html;charset=UTF-8");
 
-		System.out.println("연결됨?4");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			System.out.println("클래스가 없습니다.");
 			e.printStackTrace();
 		}
-		System.out.println("연결됨?3");
+
 		//DB정보 가져오기, mysql의 테이블이름 써줘야함. (?앞쪽에)
 		String url = "jdbc:mysql://127.0.0.1:3306/JSP_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";		
 		String user = "root";
 		String password = "";
 		
 		Connection conn = null;
-		System.out.println("연결됨?2");
+
 		try {
 			conn = DriverManager.getConnection(url, user, password);
 //			conn = DriverManager.getConnection(url, "root", "");
@@ -46,11 +45,18 @@ public class ArticleListesvlet extends HttpServlet {
 			//dbutil쪽에서는 Secsql로 받는데 여기서는 string을 써줘서
 			//어느쪽이든 선택해서 맞춰줘야함.
 			SecSql sql = SecSql.from("SELECT * FROM article;");
-			System.out.println("연결됨?1");
+
 			List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql);
 			
 			//객체정보를 스트링으로 쭉 보여주겠다.
 			response.getWriter().append(articleRows.toString());
+			//jsp쪽으로 DB서버의 아티클 정보를 요청해서 넘겨줌
+			//List/Map타입의 articleRows를 그리게 넘겨준다.
+			//밑의 주소를 따라 넘겨준다.
+			request.setAttribute("articleRows", articleRows);
+			//getWriter로 바로 보여주는게 아니라 한번더 요청을한다.
+			//이 요청이 화면에 그리게 해준다. 
+			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
 		} catch (SQLException e) {
 			System.out.println("에러 : " + e);
 		} finally {
